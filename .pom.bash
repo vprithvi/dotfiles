8d8762c1858e1b952972318f385077a9971f55dc
Prithvi Raj <prithvi.raj@ufl.edu>
2012-01-31 10:29:14 -0500

    Simple script to start/stop pomodors from cli

---
+++ .pom.bash
@@ -0,0 +1,19 @@
+# use AppleScript to communicate with the Pomodoro application on OSX
+function pom {
+    case $1 in
+        start)
+            osascript -e "tell app \"Pomodoro\" to start \"${*}\" duration 25 break 5"
+            ;;
+        stop)
+            osascript -e "tell app \"Pomodoro\" to reset"
+            ;;
+        pause)
+            osascript -e "tell app \"Pomodoro\" to external interrupt"
+            ;;
+        resume)
+            osascript -e "tell app \"Pomodoro\" to resume"
+            ;;
+        *)
+            echo "${0} {start task desc…|stop|pause|resume}"
+    esac
+}
